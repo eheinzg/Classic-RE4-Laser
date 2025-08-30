d77685f6d2b776a47c87597d861ece0f6135acf6
eheinzg <eheinzg@gmail.com>
2025-08-30 11:31:38 -0400

    Add files via upload

--- utility/CastRays.lua
+++ utility/CastRays.lua
@@ -1,4 +1,64 @@
-local statics = require("utility/Statics")
+-- Inline Statics so this module is self-contained and doesn't require utility/Statics. I referenced this from Praydog's utility scripts
+local Statics = {}
+
+function Statics.generate(typename, double_ended)
+  local double_ended = double_ended or false
+
+  local t = sdk.find_type_definition(typename)
+  if not t then return {} end
+
+  local fields = t:get_fields()
+  local enum = {}
+
+  for i, field in ipairs(fields) do
+    if field:is_static() then
+      local name = field:get_name()
+      local raw_value = field:get_data(nil)
+
+      -- Uncomment for debugging
+      -- log.info(name .. " = " .. tostring(raw_value))
+
+      enum[name] = raw_value
+
+      if double_ended then
+        enum[raw_value] = name
+      end
+    end
+  end
+
+  return enum
+end
+
+function Statics.generate_global(typename)
+  -- Split typename on '.' to build nested global tables
+  local parts = {}
+
+  for part in typename:gmatch("[^%.]+") do
+    table.insert(parts, part)
+  end
+
+  local global = _G
+  for i, part in ipairs(parts) do
+    if not global[part] then
+      global[part] = {}
+    end
+
+    global = global[part]
+  end
+
+  if global ~= _G then
+    local static_class = Statics.generate(typename)
+
+    for k, v in pairs(static_class) do
+      global[k] = v
+      global[v] = k
+    end
+  end
+
+  return global
+end
+
+local statics = Statics
 
 local CastRays = {}
 
